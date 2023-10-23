8d0caf8651ef707e85a437465a27e0f5ecbe3db2
Aylin <aylinyildiz1628@icloud.com>
2023-10-23 22:14:24 +0300

    Enums hw added

---
+++ Week 1/Cohorts/Enums/Enums/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+int sıcaklık = 32;
+
+if (sıcaklık <= (int)HavaDurumu.Normal)
+    Console.WriteLine("Hava sıcak olmayabilir.");
+else if (sıcaklık >= (int)HavaDurumu.Sıcak)
+    Console.WriteLine("Hava çok sıcak.");
+else if (sıcaklık >= (int)HavaDurumu.CokSıcak)
+    Console.WriteLine("Hava fazla sıcak.");
+
+
+
+enum Gunler
+{
+    Pazartesi = 1,
+    Salı,
+    Çarşamba,
+    Perşembe,
+    Cuma = 23,
+    Cumartesi,
+    Pazar
+}
+
+enum HavaDurumu
+{
+    Soguk = 5,
+    Normal = 20,
+    Sıcak = 25,
+    CokSıcak = 30
+}
