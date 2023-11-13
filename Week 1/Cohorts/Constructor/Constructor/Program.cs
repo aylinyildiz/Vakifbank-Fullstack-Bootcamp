04d4d780f2cd87b89379b9cc27a75a5c1a40ca1b
Aylin <aylinyildiz1628@icloud.com>
2023-11-13 21:40:48 +0300

    Constructor added

---
+++ Week 1/Cohorts/Constructor/Constructor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Constructor
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Calisan calisan = new Calisan("ayşe", "kara", 1236);
+        }
+    }
+
+    class Calisan
+    {
+        public string Ad;
+        public string Soyad;
+        public int No;
+
+        public Calisan(string ad, string soyad, int no)
+        {
+            this.Ad= ad;
+            this.Soyad= soyad;
+            this.No = no;
+        }
+    }
+}
